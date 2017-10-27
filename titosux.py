96ae30c667616af540ce568226cd69b635af7652
talhautsc <talha.khatri@mail.utoronto.ca>
2017-10-27 14:27:57 -0400

    changed lines 3 & 6

--- titosux.py
+++ titosux.py
@@ -1,9 +1,9 @@
 def foo(int1, int2):
     if int1 < 2: return 2*2
-    elif int1 > 2: return 2*3
+    elif int1 > 900: return 2*3
     elif int1 == 2 and int2 == 2: return 1
     if int2 >= 0: return 11
-    else: return ("talha")
+    else: return int1-int2
 
 print(foo(5,4))
 print("julian is a fatboi")
